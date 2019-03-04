c335112fd868f4f678192f256bbbac7fcafe71aa
Jezer <jezer.chilel@gmail.com>
2019-03-04 16:33:36 -0600

    Add modal to create new locations

--- client/src/components/Controls/index.js
+++ client/src/components/Controls/index.js
@@ -1,5 +1,16 @@
 import React, { Fragment } from 'react';
-import { Button } from 'reactstrap';
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Form,
+  FormGroup,
+  Col,
+  Label,
+  Input
+} from 'reactstrap';
 import PropTypes from 'prop-types';
 import { NavigationControl } from 'react-map-gl';
 
@@ -17,17 +28,70 @@ const buttonStyle = {
   padding: 10
 };
 
-function Controls({ updateViewport }) {
-  return (
-    <Fragment>
-      <div style={navStyle}>
-        <NavigationControl onViewportChange={updateViewport} />
-      </div>
-      <div style={buttonStyle}>
-        <Button color="primary">Add Location</Button>
-      </div>
-    </Fragment>
-  );
+class Controls extends React.Component {
+  state = {
+    modal: false
+  };
+
+  toggle = () => {
+    this.setState(prevState => ({
+      modal: !prevState.modal
+    }));
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div style={navStyle}>
+          <NavigationControl onViewportChange={this.props.updateViewport} />
+        </div>
+        <div style={buttonStyle}>
+          <Button onClick={this.toggle} color="primary">Add Location</Button>
+        </div>
+        <Modal 
+          isOpen={this.state.modal} 
+          toggle={this.toggle} 
+          className={this.props.className}
+          backdrop="static"
+        >
+          <ModalHeader toggle={this.toggle}>Adding new location</ModalHeader>
+          <ModalBody>
+            <Form>
+              <FormGroup row>
+                <Label for="name" sm={2}>Name</Label>
+                <Col sm={10}>
+                  <Input type="text" name="name" id="name" placeholder="Location name" />
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label for="latitude" sm={2}>Latitude</Label>
+                <Col sm={10}>
+                  <Input type="text" name="latitude" id="latitude" placeholder="Location latitude" />
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label for="longitude" sm={2}>Longitude</Label>
+                <Col sm={10}>
+                  <Input type="text" name="longitude" id="longitude" placeholder="Location longitude" />
+                </Col>
+              </FormGroup>
+              <FormGroup>
+                <Label for="exampleSelect">Select status</Label>
+                <Input type="select" name="select" id="exampleSelect">
+                  <option>Open</option>
+                  <option>Close</option>
+                </Input>
+              </FormGroup>
+            </Form>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="success" onClick={this.toggle}>Save</Button>{' '}
+            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
+      </Fragment>
+    );
+  }
 }
 
 Controls.propTypes = {
